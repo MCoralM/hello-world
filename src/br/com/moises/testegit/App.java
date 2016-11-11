fb1580a8a7ca4c7b7d869ce119eaa25ab440b85e
Moises Coral Milanez <Moises Coral Milanez@172.16.100.158>
2016-11-11 20:54:11 -0200

    Commit inicial do projeto.

---
+++ src/br/com/moises/testegit/App.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.moises.testegit;
+
+/**
+ *
+ * @author Moises Coral Milanez
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Adciionando projeto no Git");
+    }
+}
